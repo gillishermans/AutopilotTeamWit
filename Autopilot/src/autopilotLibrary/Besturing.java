c3f82b5fbd70f4711d92ef829aa5c46326f40f54
gillis <gillis@10.43.103.231>
2017-10-30 17:16:26 +0100

    Ratio autopilot

--- Autopilot/src/autopilotLibrary/Besturing.java
+++ Autopilot/src/autopilotLibrary/Besturing.java
@@ -27,72 +27,90 @@ public class Besturing {
 		float distance = beeldherkenning.distanceToObject(beeldherkenning.getRadius()[0]);
 		double horizontalAngle = beeldherkenning.horizontalAngle(beeldherkenning.getCenter());	
 		double verticalAngle = beeldherkenning.verticalAngle(beeldherkenning.getCenter());
-		System.out.print("vertical angle" + verticalAngle);
 		
-		
-		
-		
-	//DEMO 1 - 0 thrust -> valt
-//		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
-//		return outputs;
-		
-	//DEMO 2	
-//		leftWingInclination = (float) Math.PI/18;
-//		rightWingInclination = (float) Math.PI/18;
-//		//horStabInclination = (float) Math.PI/18;
-//		thrust=(float) (2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1*Math.pow(9,2));//*Math.pow(100,2));
-//		
-//		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
-//		return outputs;
 		
 	//DEMO 3	
+//		if (verticalAngle == -0f || (verticalAngle < (Math.PI)/(180))&& verticalAngle > -(Math.PI)/(180)){
+//			rightWingInclination = (float) (Math.PI/20);
+//			leftWingInclination = (float) (Math.PI/20);
+//			horStabInclination = 0f;
+//			
+//		}
+//		else if (verticalAngle >0 ){
+//			 rightWingInclination+=(float) (Math.PI/30.0);
+//			 leftWingInclination+=(float) (Math.PI/30.0);
+//			 
+//			 System.out.println("winginclination" + leftWingInclination);
+//			 
+//			 if (rightWingInclination > Math.PI/4) {
+//				 rightWingInclination = (float) (Math.PI/4);
+//				 leftWingInclination = (float) (Math.PI/4);
+//				 horStabInclination = (float) (Math.PI/15);
+//			 }
+//		}
+////		else if (verticalAngle < -(0.05*Math.PI)){
+////			rightWingInclination = (float) (Math.PI/20);
+////			leftWingInclination = (float) (Math.PI/20);
+////			horStabInclination = 0f;
+////		}
+//		else{
+//			 rightWingInclination+=(float) -(Math.PI/180.0);
+//			 leftWingInclination+=(float) -(Math.PI/180.0);
+//			 horStabInclination -= Math.PI/240;
+////			 System.out.println("horstabinclination" + horStabInclination);
+////			 if (horStabInclination > 0) 
+////				 horStabInclination -= Math.PI/720;
+////			 if (rightWingInclination < -Math.PI/4) {
+////				 rightWingInclination = (float) -(Math.PI/4);
+////				 leftWingInclination = (float) -(Math.PI/4);
+////			 }	 
+//		}
+		
+		//VERTICAAL
 		if (verticalAngle == -0f){
 			rightWingInclination = (float) (Math.PI/20);
 			leftWingInclination = (float) (Math.PI/20);
 			horStabInclination = 0f;
-			System.out.println("hallo");
-			
 		}
-		else if (verticalAngle >0 ){
-			 rightWingInclination+=(float) (Math.PI/30.0);
-			 leftWingInclination+=(float) (Math.PI/30.0);
-			 
-			 System.out.println("winginclination" + leftWingInclination);
-			 
-			 if (rightWingInclination > Math.PI/4) {
-				 rightWingInclination = (float) (Math.PI/4);
-				 leftWingInclination = (float) (Math.PI/4);
-				 horStabInclination = (float) (Math.PI/15);
-			 }
+		else if (verticalAngle > 0 ){
+			float ratio =(float) ((float) verticalAngle / (Math.PI/3));
+			rightWingInclination = (float) ((float) 2*ratio*(Math.PI/6));
+			leftWingInclination = (float) ((float) 2*ratio*(Math.PI/6));
+			//horStabInclination = (float) ((float) 2*ratio*(-Math.PI/30));
 		}
 		
 		else{
-			 rightWingInclination+=(float) -(Math.PI/150.0);
-			 leftWingInclination+=(float) -(Math.PI/150.0);
-			 System.out.println("horstabinclination" + horStabInclination);
-			 if (horStabInclination > 0) 
-				 horStabInclination -= Math.PI/720;
-			 if (rightWingInclination < -Math.PI/4) {
-				 rightWingInclination = (float) -(Math.PI/4);
-				 leftWingInclination = (float) -(Math.PI/4);
-			 }
-			 
+			float ratio =(float) ((float) verticalAngle / (Math.PI/3));
+			rightWingInclination = (float) ((float) 2*ratio*(Math.PI/4));
+			leftWingInclination = (float) ((float) 2*ratio*(Math.PI/4));
+			//horStabInclination = (float) ((float) ratio*(Math.PI/1000));
 		}
+		
+		
+		//HORIZONTAAL
+		if (horizontalAngle == -0f){
+			verStabInclination=0;
+		}
+		else if (horizontalAngle > 0 ){
+			verStabInclination= (float) -Math.PI/10; 
+		}
+		
+		else{
+			verStabInclination= (float) Math.PI/10; 
+		}
+		
+		
 		
 		// Beginsnelheid initialiseren met 100
 		// AOA=> 1
 		// thrust moet versnelling in de z-richting, veroorzaakt door vleugels teniet doen
 		
-		thrust=(float) (2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1*Math.pow(9,2));//*Math.pow(100,2));
-		//thrust = 10f;
-		//horStabInclination=0;
-		verStabInclination=0;
+		thrust=(float) Math.abs(2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1*Math.pow(9,2));//*Math.pow(100,2));
+		//thrust = 100f;
+
 		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
 		
 		return outputs;
 	}
-	
-	
-	
-	
+
 }
