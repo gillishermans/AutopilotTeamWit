424aa223f11cdf9761281cfb4665516917010742
gillis <gillis@10.43.103.231>
2017-10-30 12:05:46 +0100

    Demo

--- Autopilot/src/autopilotLibrary/Besturing.java
+++ Autopilot/src/autopilotLibrary/Besturing.java
@@ -30,25 +30,40 @@ public class Besturing {
 		float horStabInclination = 0.0f;
 		float verStabInclination = 0.0f;
 		
+	//DEMO 1 - 0 thrust -> valt
+//		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
+//		return outputs;
+		
+	//DEMO 2	
+//		leftWingInclination = (float) Math.PI/18;
+//		rightWingInclination = (float) Math.PI/18;
+//		//horStabInclination = (float) Math.PI/18;
+//		thrust=(float) (2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1*Math.pow(9,2));//*Math.pow(100,2));
+//		
+//		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
+//		return outputs;
+		
+	//DEMO 3	
+		
 		if (verticalAngle >=0 ){
-			 rightWingInclination=(float) (Math.PI/32.0);
-			 leftWingInclination=(float) (Math.PI/32.0);
+			 rightWingInclination=(float) (Math.PI/4.0);
+			 leftWingInclination=(float) (Math.PI/4.0);
 		}
 		else{
-			 rightWingInclination=(float) -(Math.PI/32.0);
-			 leftWingInclination=(float) -(Math.PI/32.0);
+			 rightWingInclination=(float) -(Math.PI/4.0);
+			 leftWingInclination=(float) -(Math.PI/4.0);
 		}
 		
 		// Beginsnelheid initialiseren met 100
 		// AOA=> 1
 		// thrust moet versnelling in de z-richting, veroorzaakt door vleugels teniet doen
 		
-		thrust=(float) (-2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1);//*Math.pow(100,2));
+		thrust=(float) (2*Math.sin(rightWingInclination)*this.config.getWingLiftSlope()*1*Math.pow(9,2));//*Math.pow(100,2));
 		horStabInclination=0;
 		verStabInclination=0;
-		AutopilotOutputs output = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
+		AutopilotOutputs outputs = new Outputs(thrust,leftWingInclination , rightWingInclination, horStabInclination, verStabInclination);
 		
-		return output;
+		return outputs;
 	}
 	
 	
