295f9efe4ce21b6627ba4cd1b52f89ba3f2b5860
zoltandhaese <zdhaese@gmail.com>
2018-05-01 17:14:56 +0200

    acos

--- Autopilot/src/autopilotLibrary/Airport.java
+++ Autopilot/src/autopilotLibrary/Airport.java
@@ -215,7 +215,7 @@ public class Airport {
 	public float getRadians (float L, float W, float centerToRunway0x, float centerToRunway0z){
 		Vector vector1 = new Vector(L,0,W); 
 		Vector vector2 = new Vector (centerToRunway0x,0,centerToRunway0z);
-		float radian = Vector.scalairProd(vector1, vector2)/(Vector.norm(vector1)*Vector.norm(vector2));
+		float radian = (float) Math.acos(Vector.scalairProd(vector1, vector2)/(Vector.norm(vector1)*Vector.norm(vector2)));
 		return radian;
 	}
 	
