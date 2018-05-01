2246c0acbea00ddf36cff7037ed86e552189a2a8
zoltandhaese <zdhaese@gmail.com>
2018-05-01 17:12:42 +0200

    test

--- Autopilot/src/autopilotLibrary/Airport.java
+++ Autopilot/src/autopilotLibrary/Airport.java
@@ -211,19 +211,14 @@ public class Airport {
 	public float centerToRunway0Z(){
 		return this.centerToRunway0Z;
 	}
-
 	
 	public float getRadians (float L, float W, float centerToRunway0x, float centerToRunway0z){
 		Vector vector1 = new Vector(L,0,W); 
 		Vector vector2 = new Vector (centerToRunway0x,0,centerToRunway0z);
 		float radian = Vector.scalairProd(vector1, vector2)/(Vector.norm(vector1)*Vector.norm(vector2));
 		return radian;
-		
-		
 	}
 	
-
-
 	public float getOrientation() {
 		return orientation;
 	}
